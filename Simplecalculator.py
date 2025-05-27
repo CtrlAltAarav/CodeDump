52b3869b0bcde6ce8ca335c53c1f2e6b90e7e038
CtrlAltAarav <xyzaarav45@gmail.com>
2025-05-27 15:19:01 +0530

    Add files via upload

---
+++ Simplecalculator.py
@@ -0,0 +1,7 @@
+# calculator.py
+a = int(input("enter the first term : "))
+b = int(input("enter the second term : "))
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
